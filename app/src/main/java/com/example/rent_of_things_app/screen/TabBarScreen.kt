92d0e71f6d973543d678ac02af50f46d2433c3e2
EvgenieBespalov <evg.bespaloff@yandex.ru>
2023-07-06 19:06:28 +0700

    Реализовал таббар и переход по нему к страницам

--- app/src/main/java/com/example/rent_of_things_app/screen/TabBarScreen.kt
+++ app/src/main/java/com/example/rent_of_things_app/screen/TabBarScreen.kt
@@ -1,20 +1,14 @@
 package com.example.rent_of_things_app.screen
 
 import android.widget.Toast
-import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.pager.HorizontalPager
-import androidx.compose.foundation.pager.PagerState
-import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.*
-import androidx.compose.material.icons.outlined.AccountCircle
 import androidx.compose.material.icons.outlined.Person
 import androidx.compose.material.icons.outlined.Search
 import androidx.compose.material.icons.outlined.ShoppingCart
@@ -24,44 +18,64 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalFocusManager
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.example.rent_of_things_app.screen.theme.ScreenSignIn
 import com.example.rent_of_things_app.screen.theme.grey
 import com.example.rent_of_things_app.screen.theme.yellowActive
-import kotlinx.coroutines.launch
 
 @Composable
-fun MyUI() {
-    val bottomMenuItemsList = prepareBottomMenu()
+fun MyUI(navController: NavHostController) {
+    /*val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = Routes.SignInScreenRoute.route) {
+        composable(Routes.SignInScreenRoute.route) {
+            ScreenSignIn()
+        }
+        composable(Routes.SignUpScreenRoute.route) {
+            ScreenSignUp()
+        }
+    }*/
+
+    //val bottomMenuItemsList = prepareBottomMenu()
     val contextForToast = LocalContext.current.applicationContext
 
     var selectedItem by remember { mutableStateOf("Поиск вещей") }
 
-    Box(modifier = Modifier
-        .fillMaxSize()
-        .background(color = Color.Yellow),
-    ) {
+//    Box(modifier = Modifier
+//        .fillMaxSize()
+//        .background(color = Color.Yellow),
+//    ) {
 
         BottomNavigation(
             modifier = Modifier
-                .align(alignment = Alignment.BottomCenter)
+                //.align(alignment = Alignment.BottomCenter)
                 .border(width = 1.dp, color = grey),
             backgroundColor = Color.White
         ) {
-            bottomMenuItemsList.forEach { menuItem ->
+            val navBackStackEntry by navController.currentBackStackEntryAsState()
+            val currentRoute = navBackStackEntry?.destination?.route
+
+
+            Constants.BottomMenuItems.forEach { menuItem ->
                 BottomNavigationItem(
-                    selected = (selectedItem == menuItem.label),
+                    selected = (currentRoute == menuItem.route),
+                    //selected = (selectedItem == menuItem.label),
                     selectedContentColor = yellowActive,
                     unselectedContentColor = grey,
                     onClick = {
-                        selectedItem = menuItem.label
+                        navController.navigate(menuItem.route)
+                        /*selectedItem = menuItem.label
                         Toast.makeText(
                             contextForToast,
                             menuItem.label,
                             Toast.LENGTH_SHORT
-                        ).show()
+                        ).show()*/
                     },
                     icon = {
                         Icon(
@@ -74,29 +88,94 @@ fun MyUI() {
                             text = menuItem.label
                         )
                     },
-                    enabled = true
+                    enabled = true,
+                    alwaysShowLabel = false
                 )
             }
         }
-    }
+    //}
 }
 
 @Preview
 @Composable
 fun MyUITest(){
-    MyUI()
+    val navController = rememberNavController()
+    Scaffold(
+        // Bottom navigation
+        bottomBar = {
+            MyUI(navController = navController)
+        },
+        content = { padding ->
+            // Navhost: where screens are placed
+            NavHostContainer(navController = navController, padding = padding)
+        }
+    )
+
 }
 
-private fun prepareBottomMenu(): List<BottomMenuItem> {
+/*private fun prepareBottomMenu(): List<BottomMenuItem> {
     val bottomMenuItemsList = arrayListOf<BottomMenuItem>()
 
-    bottomMenuItemsList.add(BottomMenuItem(label = "Поиск вещей", icon = Icons.Outlined.Search))
-    bottomMenuItemsList.add(BottomMenuItem(label = "Мои вещи", icon = Icons.Outlined.ShoppingCart))
-    bottomMenuItemsList.add(BottomMenuItem(label = "Профиль", icon = Icons.Outlined.Person))
+    bottomMenuItemsList.add(BottomMenuItem(label = "Поиск вещей", icon = Icons.Outlined.Search, { ScreenSignIn()}))
+    bottomMenuItemsList.add(BottomMenuItem(label = "Мои вещи", icon = Icons.Outlined.ShoppingCart, { ScreenSignIn()}))
+    bottomMenuItemsList.add(BottomMenuItem(label = "Профиль", icon = Icons.Outlined.Person, { ScreenSignUp()}))
 
 
     return bottomMenuItemsList
+}*/
+
+object Constants {
+    val BottomMenuItems = listOf(
+        BottomMenuItem(
+            label = "Поиск вещей",
+            icon = Icons.Outlined.Search,
+            route = "search"
+        ),
+        BottomMenuItem(
+            label = "Мои вещи",
+            icon = Icons.Outlined.ShoppingCart,
+            route = "search"
+        ),
+        BottomMenuItem(
+            label = "Профиль",
+            icon = Icons.Outlined.Person,
+            route = "profile"
+        )
+    )
 }
 
-data class BottomMenuItem(val label: String, val icon: ImageVector)
+//typealias ComposableFun = @Composable () -> Unit
 
+data class BottomMenuItem(val label: String, val icon: ImageVector, var route: String)
+
+
+@Composable
+fun NavHostContainer(
+    navController: NavHostController,
+    padding: PaddingValues
+) {
+
+    NavHost(
+        navController = navController,
+
+        // set the start destination as home
+        startDestination = "search",
+
+        // Set the padding provided by scaffold
+        modifier = Modifier.padding(paddingValues = padding),
+
+        builder = {
+
+            // route : Home
+            composable("search") {
+                ScreenSignIn()
+            }
+
+
+            // route : profile
+            composable("profile") {
+                ScreenSignUp()
+            }
+        })
+
+}
