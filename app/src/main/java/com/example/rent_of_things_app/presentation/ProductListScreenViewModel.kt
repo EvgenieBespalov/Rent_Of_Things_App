4c5810e245182d74afda5c7c20c7406d6690996b
EvgenieBespalov <evg.bespaloff@yandex.ru>
2023-07-11 12:43:15 +0700

    Добавил viewmodel для экрана Product Screen

---
+++ app/src/main/java/com/example/rent_of_things_app/presentation/ProductListScreenViewModel.kt
@@ -0,0 +1,34 @@
+package com.example.rent_of_things_app.presentation
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.CancellationException
+import kotlinx.coroutines.launch
+
+class ProductListScreenViewModel(): ViewModel(){
+    private val _state: MutableLiveData<ProductListScreenUiState> = MutableLiveData(ProductListScreenUiState.Initial)
+    val state: LiveData<ProductListScreenUiState> = _state
+
+    fun initial() {
+        viewModelScope.launch {
+            _state.value = ProductListScreenUiState.Initial
+        }
+    }
+
+    fun getListProduct(){
+        viewModelScope.launch {
+            _state.value = ProductListScreenUiState.Loading
+
+            try {
+                _state.value = ProductListScreenUiState.Content("it")
+            } catch (rethrow: CancellationException) {
+                throw rethrow
+            } catch (ex: Exception) {
+                _state.value = ProductListScreenUiState.Error(ex.message)
+            }
+
+        }
+    }
+}
--- app/src/main/java/com/example/rent_of_things_app/screen/offer_list_screens/ProductListScreen.kt
+++ app/src/main/java/com/example/rent_of_things_app/screen/offer_list_screens/ProductListScreen.kt
@@ -11,12 +11,12 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Menu
 import androidx.compose.material.icons.outlined.Search
 import androidx.compose.runtime.*
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.graphics.painter.ColorPainter
 import androidx.compose.ui.input.nestedscroll.NestedScrollConnection
 import androidx.compose.ui.input.nestedscroll.NestedScrollSource
 import androidx.compose.ui.input.nestedscroll.nestedScroll
@@ -24,19 +24,24 @@ import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import com.example.rent_of_things_app.R
+import com.example.rent_of_things_app.presentation.ProductListScreenUiState
+import com.example.rent_of_things_app.presentation.ProductListScreenViewModel
 import com.example.rent_of_things_app.screen.offer_list_screens.*
 import com.example.rent_of_things_app.screen.theme.*
+import org.koin.androidx.compose.koinViewModel
 import kotlin.math.roundToInt
 
 
 @Composable
-fun ScreenListOfRentalOffers(){
+fun ProductListScreen(
+    viewModel: ProductListScreenViewModel = koinViewModel()
+){
     val toolbarHeightPx = with(LocalDensity.current) { toolbarHeight.roundToPx().toFloat() }
     val toolbarOffsetHeightPx = remember { mutableStateOf(0f) }
-
     val nestedScrollConnection = remember {
         object : NestedScrollConnection {
             override fun onPreScroll(available: Offset, source: NestedScrollSource): Offset {
@@ -49,6 +54,9 @@ fun ScreenListOfRentalOffers(){
         }
     }
 
+    val state by viewModel.state.observeAsState(ProductListScreenUiState.Content("ii"))
+    //viewModel.getListProduct()
+
     Box(
         Modifier
             .fillMaxSize()
@@ -56,13 +64,35 @@ fun ScreenListOfRentalOffers(){
             .background(color = Color.White),
         contentAlignment = Alignment.TopCenter
     ) {
-        ListOfItems()
+
+        when(state){
+            ProductListScreenUiState.Initial    -> viewModel.getListProduct()
+            ProductListScreenUiState.Loading    -> ScreenLoadind()
+            is ProductListScreenUiState.Content -> ProductListListOfProducts()
+            is ProductListScreenUiState.Error   -> ScreenError(errorText = (state as ProductListScreenUiState.Error).message.orEmpty())
+        }
+
         Box(
             modifier = Modifier
                 .height(toolbarHeight)
                 .offset { IntOffset(x = 0, y = toolbarOffsetHeightPx.value.roundToInt()) },
         ){
             TabBar()
+        }
+    }
+}
+
+@Composable
+fun ProductListListOfProducts(){
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(2),
+        userScrollEnabled = userScrollEnabled.value
+    ) {
+        items(100) { index ->
+            ItemOfList(
+                nameThings = "Name $index",
+                price = "Price $index"
+            )
         }
     }
 }
@@ -78,16 +108,14 @@ fun TabBar(){
         contentAlignment = Alignment.Center
     ){
         Column(
-            modifier = Modifier
-                .padding(paddingTabBar)
+            modifier = Modifier.padding(paddingTabBar)
         ){
             //SearchBarThings()
             PullOutPanel()
             IconButton(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .height(sizeIconExpandButton)//.background(Color.Red)
-                ,
+                    .height(sizeIconExpandButton),
                 onClick = {
                     when(pullOutPanelExtended.value){
                         false -> {
@@ -110,9 +138,7 @@ fun TabBar(){
                             false -> R.drawable.icon_expand_more
                         }
                     ),
-                    modifier = Modifier
-                        .height(sizeIconExpand)//.background(Color.Blue)
-                    ,
+                    modifier = Modifier.height(sizeIconExpand),
                     contentDescription = null,
                     tint = yellowActive
                 )
@@ -124,7 +150,7 @@ fun TabBar(){
 @Composable
 fun PullOutPanel(){
     Box(modifier = Modifier
-        .height(sizePullOutPanel.value)//.background(Color.Yellow)
+        .height(sizePullOutPanel.value)
     ){
         //Text("gggggggg")
     }
@@ -139,7 +165,7 @@ fun PullOutPanelPreview(){
 @Preview
 @Composable
 fun ScreenListOfRentalOffersPreview(){
-    ScreenListOfRentalOffers()
+    ProductListScreen()
 }
 
 @Preview
@@ -148,38 +174,3 @@ fun TabBarPreview(){
     TabBar()
 }
 
-@Composable
-fun SearchBarThings(){
-    var textField by remember { mutableStateOf("") }
-
-    OutlinedTextField(
-        modifier = Modifier
-            .fillMaxWidth()
-            .height(sizeSearchLine)
-            .graphicsLayer {
-                clip = true
-                shape = RoundedCornerShape(shape10)
-            },
-        leadingIcon = { Icon(Icons.Outlined.Search, contentDescription = null) },
-        trailingIcon = {
-            IconButton(onClick = { Log.d("Click", "IconExample")}) {
-                Icon(Icons.Outlined.Menu, contentDescription = null)
-            }
-        },
-        value = textField,
-        onValueChange = { textField = it },
-        colors = TextFieldDefaults.outlinedTextFieldColors(
-            textColor = Color.Black,
-            backgroundColor = Color.White,
-            placeholderColor = greyText,
-            focusedBorderColor = Color.Black,
-            unfocusedBorderColor = Color.Black,
-            disabledBorderColor = Color.Black,
-            errorBorderColor = Color.Black,
-            leadingIconColor = grey,
-            trailingIconColor = yellowActive
-        ),
-        shape = MaterialTheme.shapeScheme.shape10,
-        textStyle = TextStyle(fontSize = fontTextFieldSignScreen),
-    )
-}
