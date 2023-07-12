8349cfbe660b1d9070b233655d0a8a616b09bff8
EvgenieBespalov <evg.bespaloff@yandex.ru>
2023-07-12 19:57:02 +0700

    Добавил код для работы с апи через ретрофит

--- app/src/main/java/com/example/rent_of_things_app/MyApplication.kt
+++ app/src/main/java/com/example/rent_of_things_app/MyApplication.kt
@@ -1,6 +1,7 @@
 package com.example.rent_of_things_app
 
 import android.app.Application
+import com.example.rent_of_things_app.di.provideNetworkModule
 import com.example.rent_of_things_app.di.providePresentationModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
@@ -13,7 +14,7 @@ class MyApplication : Application() {
         startKoin {
             androidContext(this@MyApplication)
             modules(
-                //provideNetworkModule(),
+                provideNetworkModule(),
                 //provideDataModule(),
                 //provideDomainModule(),
                 providePresentationModule(),
