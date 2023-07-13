598c9c92cbfd6a9fe7c5a3805955dab125c2889c
EvgenieBespalov <evg.bespaloff@yandex.ru>
2023-07-13 14:58:41 +0700

    Добавил репозиторий

---
+++ app/src/main/java/com/example/rent_of_things_app/domain/repository/UserRepository.kt
@@ -0,0 +1,7 @@
+package com.example.rent_of_things_app.domain.repository
+
+import com.example.rent_of_things_app.domain.entity.UserEntity
+
+interface UserRepository {
+    suspend fun userRegistration(): UserEntity
+}
