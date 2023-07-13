7f747794383559e906a17cc51a12cfb3072d67b0
EvgenieBespalov <evg.bespaloff@yandex.ru>
2023-07-13 14:35:25 +0700

    Добавил сущность юзера

---
+++ app/src/main/java/com/example/rent_of_things_app/domain/entity/UserEntity.kt
@@ -0,0 +1,14 @@
+package com.example.rent_of_things_app.domain.entity
+
+import java.util.Date
+
+data class UserEntity(
+    val id: String,
+    val email: String,
+    val socialNetworks: List<String>,
+    val name: String,
+    val surname: String,
+    val middleName: String,
+    val registrationDate: Date,
+    val admin: Boolean
+)
