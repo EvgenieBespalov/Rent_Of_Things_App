f512a739366b24af561fd48ff5919e5c05d5fc68
EvgenieBespalov <evg.bespaloff@yandex.ru>
2023-07-13 14:16:13 +0700

    Добавил реквест модель юзера при регистрации

---
+++ app/src/main/java/com/example/rent_of_things_app/data/model/user/UserRegistrationRequestModel.kt
@@ -0,0 +1,18 @@
+package com.example.rent_of_things_app.data.model.user
+
+import com.google.gson.annotations.SerializedName
+
+data class UserRegistrationRequestModel(
+    @SerializedName("email")
+    val email: String,
+    @SerializedName("name")
+    val name: String,
+    @SerializedName("surname")
+    val surname: String,
+    @SerializedName("middle_name")
+    val middleName: String,
+    @SerializedName("password")
+    val password: String,
+    @SerializedName("social_networks")
+    val socialNetworks: List<String>
+)
