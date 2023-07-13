8766c13f34ab7d752ea2ba81edb5c02b51d95f51
EvgenieBespalov <evg.bespaloff@yandex.ru>
2023-07-14 04:11:31 +0700

    Добавил модель и сущность для категорий товаров

---
+++ app/src/main/java/com/example/rent_of_things_app/data/model/ProductTypeModel.kt
@@ -0,0 +1,8 @@
+package com.example.rent_of_things_app.data.model
+
+import com.google.gson.annotations.SerializedName
+
+data class ProductTypeModel(
+    @SerializedName("product_type")
+    val productName: List<String>
+)
