1753aaeeeb8f44605cd09ba0cd7d1d7324dedb85
EvgenieBespalov <evg.bespaloff@yandex.ru>
2023-07-14 19:14:04 +0700

    Добавил юзкейс для поиска товаров по категориям

--- app/src/main/java/com/example/rent_of_things_app/presentation/ProductListScreenViewModel.kt
+++ app/src/main/java/com/example/rent_of_things_app/presentation/ProductListScreenViewModel.kt
@@ -6,12 +6,14 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.rent_of_things_app.domain.usecase.GetAllProductUseCase
 import com.example.rent_of_things_app.domain.usecase.GetProductTypeUseCase
+import com.example.rent_of_things_app.domain.usecase.GetProductsByTypeUseCase
 import kotlinx.coroutines.CancellationException
 import kotlinx.coroutines.launch
 
 class ProductListScreenViewModel(
     private val getAllProductUseCase: GetAllProductUseCase,
-    private val getProductTypeUseCase: GetProductTypeUseCase
+    private val getProductTypeUseCase: GetProductTypeUseCase,
+    private val getProductsByTypeUseCase: GetProductsByTypeUseCase
 ): ViewModel(){
     private val _state: MutableLiveData<ProductListScreenUiState> = MutableLiveData(ProductListScreenUiState.Initial)
     val state: LiveData<ProductListScreenUiState> = _state
@@ -19,6 +21,23 @@ class ProductListScreenViewModel(
     fun initial() {
         viewModelScope.launch {
             _state.value = ProductListScreenUiState.Initial
+        }
+    }
+
+    fun getAllProductType(){
+        viewModelScope.launch {
+            _state.value = ProductListScreenUiState.Loading
+
+            try {
+                val allProduct = getAllProductUseCase()
+                val productType = getProductTypeUseCase()
+                _state.value = ProductListScreenUiState.Content(allProduct, productType)
+            } catch (rethrow: CancellationException) {
+                throw rethrow
+            } catch (ex: Exception) {
+                _state.value = ProductListScreenUiState.Error(ex.message)
+            }
+
         }
     }
 
@@ -30,6 +49,23 @@ class ProductListScreenViewModel(
                 val allProduct = getAllProductUseCase()
                 val productType = getProductTypeUseCase()
                 _state.value = ProductListScreenUiState.Content(allProduct, productType)
+            } catch (rethrow: CancellationException) {
+                throw rethrow
+            } catch (ex: Exception) {
+                _state.value = ProductListScreenUiState.Error(ex.message)
+            }
+
+        }
+    }
+
+    fun getProductsByType(productType: String){
+        viewModelScope.launch {
+            _state.value = ProductListScreenUiState.Loading
+
+            try {
+                val productByType = getProductsByTypeUseCase(productType)
+                val productType = getProductTypeUseCase()
+                _state.value = ProductListScreenUiState.Content(productByType, productType)
             } catch (rethrow: CancellationException) {
                 throw rethrow
             } catch (ex: Exception) {
